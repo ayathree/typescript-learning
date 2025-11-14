df9865bbd3465c00e11a194e0f92e1aa58fa7aa6
ayathree <nobanitaayathree333@gmail.com>
2025-11-14 23:47:56 +0600

    learn about static in ts

---
+++ src/static.ts
@@ -0,0 +1,23 @@
+class Counter{
+    static count : number=0;
+
+    static increment(){
+        return (Counter.count=Counter.count+1);
+    }
+    static decrement(){
+        return (Counter.count=Counter.count-1);
+    }
+}
+// const instance1 = new Counter();//ekta memory
+// console.log(instance1.increment())
+// console.log(instance1.increment())
+// console.log(instance1.increment())
+
+// const instance2 = new Counter();//different memory
+// console.log(instance2.increment())
+// console.log(instance2.increment())
+
+
+console.log(Counter.increment())
+console.log(Counter.increment())
+console.log(Counter.increment())
